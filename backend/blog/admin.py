82c99052d7bb81cee44f8c88044b8006e526210d
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-08-09 17:13:41 +0200

    removed unnecessary code

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -17,9 +17,6 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostAdmin)
 
-fields = ( 'image_tag', )
-readonly_fields = ('image_tag',)
-
 admin.site.register(Comment)
 admin.site.register(CommentLike)
 admin.site.register(PostLike)
