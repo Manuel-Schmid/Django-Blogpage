9baa4fee4c1218449a0fecff7a4f99c0b96b76c0
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-08-19 17:41:43 +0200

    start making post-data dynamic

--- frontend/blog/src/components/PostDetail.vue
+++ frontend/blog/src/components/PostDetail.vue
@@ -1,63 +1,83 @@
 <script setup>
-import { ref } from 'vue'; // defineProbs
+import { ref, watch } from 'vue'; // defineProbs
 import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
 
+let post = ref(null);
+
 const response = useQuery(gql`
-      query getPostById {
-          postById(id: 1) {
-            title
+    query getPostById {
+        postById(id: 2) {
+          title
+          text
+          image
+          dateCreated
+          category {
+            name
+          },
+          owner {
+            firstName
+            lastName
+          },
+          tags {
+              name
           }
+        }
       }
-    `);
-console.log(response.result);
+      `);
 
-const postData = {
-  data: {
-    postById: {
-      title: 'Alea iacta est',
-      text: 'Phasellus ipsum nulla, lobortis quis sem pharetra, cursus interdum dui. Fusce turpis eros, pretium non metus luctus, pellentesque maximus dolor. Aliquam erat volutpat. Sed nec interdum lorem. Nullam eget eros vitae libero interdum accumsan. Integer consequat tellus vel ipsum pulvinar pharetra. In tristique, justo imperdiet sagittis eleifend, augue metus condimentum quam, ut convallis sapien libero quis elit. Nulla lobortis nisl blandit nulla dapibus, vitae facilisis diam blandit. Fusce mollis at arcu vel fringilla. Cras iaculis lobortis ex, eu lobortis mi hendrerit id. Etiam feugiat at ante sed condimentum. Donec gravida lectus ut ullamcorper tempus. Pellentesque quis eros id leo mollis hendrerit. Vivamus vel leo mollis, facilisis velit sodales, ultricies sapien. Aliquam in ante sit amet eros ultricies interdum accumsan posuere dui. Quisque at massa tristique, finibus dolor cursus, lacinia orci. Sed in consectetur ligula. Etiam id mi quis nisi eleifend consectetur sed id urna. Donec feugiat massa sed ante gravida pharetra nec condimentum leo. In viverra dui eu arcu fringilla, non cursus odio vulputate. Morbi volutpat sed metus sit amet tincidunt.',
-      image: 'images/junction.jpeg',
-      dateCreated: '2022-08-15T15:00:57.031000+00:00',
-      category: {
-        name: 'Politics'
-      },
-      owner: {
-        firstName: 'Winston',
-        lastName: 'Wolf',
-      },
-      tags: [
-        {name: "lorem"},
-        {name: "ipsum"},
-        {name: "dolor"},
-        {name: "sit"},
-        {name: "amet"},
-      ]
-    }
-  }
-};
+watch(() => response, () =>  {
+  console.log(response.result.value)
+  post = response.result.value;
+  console.log(post)
+})
 
 
 
 
-const apiMediaURL = 'http://api.my-app.lo/media/';
+// const title = ref(postData.postById.title);
+// const text = ref(postData.postById.text);
+// const category = ref(postData.postById.category.name);
+// const owner = ref(postData.postById.owner.firstName + ' ' + postData.postById.owner.lastName);
+// const tags = ref(postData.postById.tags);
+// const imageURL = ref(process.env.VUE_APP_MEDIA_URL + postData.postById.image);
+//
+// let options = { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit' };
+// const date = ref(new Date(postData.postById.dateCreated).toLocaleDateString("en-GB", options));
 
-const title = ref(postData.data.postById.title);
-const text = ref(postData.data.postById.text);
-const category = ref(postData.data.postById.category.name);
-const owner = ref(postData.data.postById.owner.firstName + ' ' + postData.data.postById.owner.lastName);
-const tags = ref(postData.data.postById.tags);
-const imageURL = ref(apiMediaURL + postData.data.postById.image);
 
+// const postData = {
+//   data: {
+//     postById: {
+//       title: 'Alea iacta est',
+//       text: 'Phasellus ipsum nulla, lobortis quis sem pharetra, cursus interdum dui. Fusce turpis eros, pretium non metus luctus, pellentesque maximus dolor. Aliquam erat volutpat. Sed nec interdum lorem. Nullam eget eros vitae libero interdum accumsan. Integer consequat tellus vel ipsum pulvinar pharetra. In tristique, justo imperdiet sagittis eleifend, augue metus condimentum quam, ut convallis sapien libero quis elit. Nulla lobortis nisl blandit nulla dapibus, vitae facilisis diam blandit. Fusce mollis at arcu vel fringilla. Cras iaculis lobortis ex, eu lobortis mi hendrerit id. Etiam feugiat at ante sed condimentum. Donec gravida lectus ut ullamcorper tempus. Pellentesque quis eros id leo mollis hendrerit. Vivamus vel leo mollis, facilisis velit sodales, ultricies sapien. Aliquam in ante sit amet eros ultricies interdum accumsan posuere dui. Quisque at massa tristique, finibus dolor cursus, lacinia orci. Sed in consectetur ligula. Etiam id mi quis nisi eleifend consectetur sed id urna. Donec feugiat massa sed ante gravida pharetra nec condimentum leo. In viverra dui eu arcu fringilla, non cursus odio vulputate. Morbi volutpat sed metus sit amet tincidunt.',
+//       image: 'images/junction.jpeg',
+//       dateCreated: '2022-08-15T15:00:57.031000+00:00',
+//       category: {
+//         name: 'Politics'
+//       },
+//       owner: {
+//         firstName: 'Winston',
+//         lastName: 'Wolf',
+//       },
+//       tags: [
+//         {name: "lorem"},
+//         {name: "ipsum"},
+//         {name: "dolor"},
+//         {name: "sit"},
+//         {name: "amet"},
+//       ]
+//     }
+//   }
+// };
 
-let options = { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit' };
-const date = ref(new Date(postData.data.postById.dateCreated).toLocaleDateString("en-GB", options));
 
 </script>
 
 
 <template>
-  <div class="post-container">
+  <div class="post-container" v-if="post" >
+    <p>{{ post }}</p>
     <div class="post">
       <div class="post-content">
         <div class="post-header">
