aa4f84ca5eaca250795d12bb75c54b0d086de67e
Stefan Suter <stefan.suter@liip.ch>
2022-08-24 11:11:32 +0200

    vite: add prettier

--- blogapp/src/components/HelloWorld.vue
+++ blogapp/src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -16,7 +16,6 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
