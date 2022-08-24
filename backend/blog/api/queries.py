421e7aa88f4e737443cd0406eec89a258cdcaa47
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-08-24 16:34:25 +0200

    MBP-010/FE: add query for fetching posts filtered by tag

--- backend/blog/api/queries.py
+++ backend/blog/api/queries.py
@@ -1,4 +1,6 @@
 import graphene
+from taggit.models import Tag
+
 from ..models import Category, Post, User
 from .types import Post as PostType, Category as CategoryType, User as UserType
 
@@ -6,7 +8,7 @@ from .types import Post as PostType, Category as CategoryType, User as UserType
 class Query(graphene.ObjectType):
     categories = graphene.List(CategoryType)
     category_by_id = graphene.Field(CategoryType, id=graphene.ID())
-    posts = graphene.List(PostType, category_slug=graphene.String())
+    posts = graphene.List(PostType, category_slug=graphene.String(), tag_slug=graphene.String())
     post_by_slug = graphene.Field(PostType, slug=graphene.String())
     users = graphene.List(UserType)
     user_by_id = graphene.Field(UserType, id=graphene.ID())
@@ -24,12 +26,19 @@ class Query(graphene.ObjectType):
         return User.objects.get(pk=id)
 
     def resolve_posts(root, info, **kwargs):
-        slug = kwargs.get('category_slug', None)
-        if slug is not None:
+        category_slug = kwargs.get('category_slug', None)
+        if category_slug is not None:
             return Post.objects \
                 .select_related('category', 'owner') \
                 .prefetch_related('tags', 'owner__posts', 'owner__posts__tags', 'owner__posts__category') \
-                .filter(category__slug=slug)
+                .filter(category__slug=category_slug)
+        tag_slug = kwargs.get('tag_slug', None)
+        if tag_slug is not None:
+            tag_slug_id = Tag.objects.get(slug=tag_slug)
+            return Post.objects \
+                .select_related('category', 'owner') \
+                .prefetch_related('tags', 'owner__posts', 'owner__posts__tags', 'owner__posts__category') \
+                .filter(tagged_items__tag_id=tag_slug_id)
         return Post.objects \
             .select_related('category', 'owner') \
             .prefetch_related('tags', 'owner__posts', 'owner__posts__tags', 'owner__posts__category') \
