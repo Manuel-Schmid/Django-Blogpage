4c1069cf5cae5a693fa8feed12dcd818939c2cd9
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-08-24 13:03:08 +0200

    restructure directories

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp, provide, h } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import { DefaultApolloClient } from "@vue/apollo-composable";
