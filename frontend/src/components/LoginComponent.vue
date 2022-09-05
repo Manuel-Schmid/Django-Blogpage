0a3014da95631503dd37288de493fa2c2b3f2264
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-09-05 13:47:29 +0200

    MBP-020: resolve comments

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -65,28 +65,28 @@ export default {
                 required=""
               />
             </div>
-            <div class="flex items-center justify-between">
-              <div class="flex items-start"></div>
-              <a
-                href="#"
-                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >Forgot password?</a
-              >
-            </div>
+            <!--            <div class="flex items-center justify-between">-->
+            <!--              <div class="flex items-start"></div>-->
+            <!--              <a-->
+            <!--                href="#"-->
+            <!--                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"-->
+            <!--                >Forgot password?</a-->
+            <!--              >-->
+            <!--            </div>-->
             <button
               @click="submitLogin"
               class="w-full text-white bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               Sign in
             </button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don’t have an account yet?
-              <a
-                href="#"
-                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >Sign up</a
-              >
-            </p>
+            <!--            <p class="text-sm font-light text-gray-500 dark:text-gray-400">-->
+            <!--              Don’t have an account yet?-->
+            <!--              <a-->
+            <!--                href="#"-->
+            <!--                class="font-medium text-primary-600 hover:underline dark:text-primary-500"-->
+            <!--                >Sign up</a-->
+            <!--              >-->
+            <!--            </p>-->
           </form>
         </div>
       </div>
--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import gql from "graphql-tag";
 import { apolloClient } from "../api/client";
+import router from "../router/router";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
@@ -30,6 +31,7 @@ export const useAuthStore = defineStore("auth", {
       });
       this.authToken = response.data.tokenAuth.token;
       this.username = response.data.tokenAuth.payload.username;
+      await router.push({ name: "posts" });
     },
     persist: {
       enabled: true,
