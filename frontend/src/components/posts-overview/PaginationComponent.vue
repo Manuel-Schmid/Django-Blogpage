6ce825c279ff859074efef06089ae5f8617a4712
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-09-14 08:47:22 +0200

    MBP-39: add support for large numbers of pages to PaginationComponent

--- frontend/src/components/posts-overview/PaginationComponent.vue
+++ frontend/src/components/posts-overview/PaginationComponent.vue
@@ -5,17 +5,54 @@ export default {
   name: "PaginationComponent",
   props: ["numPostPages", "activePage"],
 
-  setup() {
+  setup(props: { numPostPages: number; activePage: number }) {
+    let numPostPagesList = Array.from(
+      { length: props.numPostPages },
+      (x, i) => ++i
+    );
+
+    let paginationPageNums = slicePaginationPageNums(
+      numPostPagesList,
+      props.activePage
+    );
+
+    let firstPage = paginationPageNums.includes(1) ? false : 1;
+    let lastPage = paginationPageNums.includes(props.numPostPages)
+      ? false
+      : props.numPostPages;
+
     const route = useRoute();
-    return { route };
+    return { route, paginationPageNums, firstPage, lastPage };
   },
 };
+
+function slicePaginationPageNums(numPostPages: any, activePage: number) {
+  return activePage > numPostPages.length - 5 && numPostPages.length > 5
+    ? numPostPages.slice(numPostPages.length - 9, numPostPages.length)
+    : numPostPages.length > 5 && activePage > 5
+    ? numPostPages.slice(activePage - 4, activePage + 4)
+    : numPostPages.slice(0, 9);
+}
 </script>
 
 <template>
   <div class="float-left w-full h-min my-10">
     <div class="m-auto w-max">
-      <div v-for="pageNr in numPostPages" class="float-left">
+      <div v-if="firstPage" class="pagination-link-wrapper mr-3">
+        <router-link
+          :to="{ name: route.name, query: { page: firstPage } }"
+          class="pagination-special-link-wrapper"
+        >
+          <span class="pagination-special-link">
+            <font-awesome-icon
+              icon="fa-solid fa-arrow-left"
+              class="float-left mr-2"
+            />
+            <span>{{ firstPage }}</span>
+          </span>
+        </router-link>
+      </div>
+      <div v-for="pageNr in paginationPageNums" class="pagination-link-wrapper">
         <span
           v-if="activePage === pageNr"
           class="pagination-link bg-gray-300 dark:bg-slate-500 cursor-default"
@@ -30,12 +67,35 @@ export default {
           {{ pageNr }}
         </router-link>
       </div>
+      <div v-if="lastPage" class="pagination-link-wrapper ml-3">
+        <router-link
+          :to="{ name: route.name, query: { page: lastPage } }"
+          class="pagination-special-link-wrapper"
+        >
+          <span class="pagination-special-link">
+            <span>{{ lastPage }}</span>
+            <font-awesome-icon
+              icon="fa-solid fa-arrow-right"
+              class="float-right ml-2"
+            />
+          </span>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .pagination-link {
-  @apply rounded-3xl w-7 h-7 text-black px-3 py-1.5 mx-2 text-center leading-7;
+  @apply rounded-3xl w-7 h-7 text-black mx-1 text-center leading-[1.85rem];
+}
+.pagination-link-wrapper {
+  @apply float-left flex items-center justify-center;
+}
+.pagination-special-link-wrapper {
+  @apply pagination-link bg-white cursor-pointer;
+}
+.pagination-special-link {
+  @apply inline-block w-max leading-5;
 }
 </style>
