f297122d3138d34ade0c46160311742afbbd3637
Manuel Schmid <70013637+Manuel-Schmid@users.noreply.github.com>
2022-09-16 10:20:25 +0200

    MBP-044: add Frontend "Forgot Password" field + function

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -65,6 +65,14 @@ export default {
                 required=""
               />
             </div>
+            <div class="flex items-center justify-between">
+              <div class="flex items-start"></div>
+              <a
+                href="#"
+                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >Forgot password?</a
+              >
+            </div>
             <button
               @click="submitLogin"
               class="w-full text-white bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
